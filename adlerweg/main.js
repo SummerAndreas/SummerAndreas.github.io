50f0531bcb770185cdf15e7d78d46c60fa3fd5e6
Summer <csaw2368@uibk.ac.at>
2019-03-27 15:41:40 +0100

    adlerweg-nz kopiert

---
+++ adlerweg/main.js
@@ -0,0 +1,25 @@
+const div = document.getElementById("map");
+const breite = div.getAttribute("data-lat");
+const laenge = div.getAttribute("data-lng");
+const titel = div.getAttribute("data-title");
+
+//console.log("Breite=",breite,"Länge=",laenge,"Titel=",titel 
+
+//Karte initialisieren
+let karte = L.map("map");
+//console.log(karte);
+
+karte.setView(
+    [breite,laenge],
+    13
+);
+
+//Open Street Map einbauen
+L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
+let pin = L.marker(
+    [breite,laenge]
+).addTo(karte);
+
+//Popup zum Pin heangen
+pin.bindPopup(titel).openPopup();
+
